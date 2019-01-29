6569a3f916558629a391ee6d7b2c63d2a8cca281
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2019-01-29 01:46:30 -0500

    Split main function into multiple functions, compiled using additional flags, added comments

--- exercises/ex01/cards.c
+++ exercises/ex01/cards.c
@@ -1,38 +1,86 @@
+/* Prompts user for card input, counts cards for
+Blackjack.
+
+Hwei-Shin Harriman
+Software systems SP19--Olin College of Engineering
+Program derived from Head First C.
+*/
 #include <stdio.h>
 #include <stdlib.h>
-int main()
-{
+
+/* Adjusts card count and prints updated total.
+
+  val: value classifying low or high value card.
+  count: total, accumulated count of the game.
+*/
+void changeCount(int val){
+  static int count;
+  if ((val > 2) && (val < 7)){
+    count++;
+  } else if (val == 10){
+    count--;
+  }
+  printf("Current count: %i\n", count);
+}
+
+/* Prompts user for input, puts reply in given buffer.
+
+  User input truncated to first two characters.
+
+  card_name: buffer where result is stored
+*/
+void userOutput(char *card_name){
+  puts("Enter the card name or 'X' to quit: ");
+  scanf("%2s", card_name);
+}
+
+/* Classifies card entry taken from userOutput() as
+  high, low, neutral, invalid, or exit condition.
+  Returns val, the result of classification.
+
+  Assumes user typed letters in caps.
+  card_name: stored result from userOutput()
+  val: classifies card. note that 10 is high, -1 is
+  used as a flag to check the exit condition.
+*/
+int checkType(char *card_name){
+  int val = 0;
+  switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+      val = 10;
+      break;
+    case 'A':
+      break;
+    case 'X':
+      val = -1;
+      break;
+    default:
+      val = atoi(card_name);
+      if ((val < 1) || (val > 10)){
+        puts("I don't understand that value!");
+      }
+    }
+    return val;
+}
+
+/* Repeatedly asks user for card, updates and prints count
+  until user types 'X' when prompted.
+
+  card_name: buffer array, stores user's latest card
+  val: result of card classification
+*/
+int main(){
   char card_name[3];
-  int count = 0;
-  do
-  {
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-      case 'K':
-      case 'Q':
-      case 'J':
-        val = 10;
-        break;
-      case 'A':
-        val = 11;
-        break;
-      case 'X':
-        continue;
-      default:
-        val = atoi(card_name);
-        if ((val < 1) || (val > 10)){
-          puts("I don't understand that value!");
-          continue;
-        }
-      }
-      if ((val > 2) && (val < 7)){
-        count++;
-      } else if (val == 10){
-        count--;
-      }
-      printf("Current count: %i\n", count);
-  } while (card_name[0] != 'X');
+  int val;
+  while (card_name[0] != 'X'){
+    userOutput(card_name);
+    val = checkType(card_name);
+    if (val == -1){
+      continue;
+    }
+    changeCount(val);
+  }
   return 0;
 }
