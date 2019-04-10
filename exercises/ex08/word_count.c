815d3ea667c4ac86303d3d7a6522b776d3b96489
Hwei-Shin Harriman <hwei.shin.harriman@gmail.com>
2019-04-10 00:29:01 -0400

    worked on HW 8

--- exercises/ex08/word_count.c
+++ exercises/ex08/word_count.c
@@ -27,10 +27,10 @@ input: ptr to word to be looked up in hash table
 */
 void update_histogram(GHashTable* hash, char* input){
   gpointer res = g_hash_table_lookup(hash, input);
-  if (res==NULL){
-    g_hash_table_insert(hash, input, GINT_TO_POINTER(1));
-  } else{
+  if (res != NULL){
     g_hash_table_replace(hash, input, GINT_TO_POINTER(res) + 1);
+  } else{
+    g_hash_table_insert(hash, input, GINT_TO_POINTER(1));
   }
 }
 
@@ -43,52 +43,50 @@ int read(GHashTable* hash, char* input){
   int c;
   FILE *inFile;
   char word[BUFFER];
-  unsigned int line = 0U;
-  unsigned int word_index = 0U;
+  int wordlen = 0;
 
   if ((inFile = fopen(input, "r"))== 0){
     fprintf(stderr, "Cannot open file\n");
     return EXIT_FAILURE;
-  } else {
-    int found_word = 0;
-
+  }
+  else {
+    int isWord = 0;
     //scan file for words
     while ((c=fgetc(inFile)) != EOF){
-      if (line == BUFFER){
-        fprintf(stderr, "line is too long, increase BUFFER_MAX_LENGTH");
-        return EXIT_FAILURE;
-      }
       //If reach end of a line, reset the counters and end the word
-      else if (c == '\n'){
-        if (found_word){
-          word[word_index] = '\0';
-          word_index = 0U;
-          found_word = 0;
+      if (c == '\n'){
+        if (isWord){
+          word[wordlen] = '\0';
+          wordlen = 0;
+          isWord = 0;
         }
-        line = 0U;
         continue;
       }
       //If find a char that is not a letter, reset counters and end
       //the current word
       if (!isalpha(c)){
-        if (found_word){
-          word[word_index] = '\0';
-          found_word = 0;
-          word_index = 0U;
+        if (isWord){
+          word[wordlen] = '\0';
+          isWord = 0;
+          wordlen = 0;
           update_histogram(hash, strdup(word));
         }
       }
       //Assume that a word is being looked at, add it to the word
       else {
-        found_word = 1;
-        word[word_index++] =(char)(tolower(c));
+        isWord = 1;
+        word[wordlen++] =(char)(tolower(c));
       }
-      line++;
     }
   }
   return 0;
 }
-
+//Started the sorting section but ran out of time
+// gpointer* sortResults(GHashTable* hash){
+//   guint size = g_hash_table_size(hash);
+//   gpointer* keys = g_hash_table_get_keys_as_array(hash, size);
+//   qsort()
+// }
 int main(int argc, char** argv){
   char *input = argv[1];
   GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
